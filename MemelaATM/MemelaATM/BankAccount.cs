92f555d771ae1a3c63ce8c1f2deee3e929062b80
Sibonelo790 <sibonelomemela517@gmail.com>
2026-03-27 10:43:47 +0200

    Created generic bank account

---
+++ MemelaATM/MemelaATM/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemelaATM
+{
+    public class BankAccount
+    {
+        //Properties
+        public string AccountNumber { get; set; }
+        public string Pin { get; set; }
+        public double Balance { get; private set; }
+
+        //Constructor to set up a new account
+        public BankAccount(string accountNumber, string pin, double balance)
+        {
+            AccountNumber = accountNumber;
+            Pin = pin;
+            Balance = balance;
+        }
+
+        //Method to deposit money
+        public void Deposit(double amount)
+        {
+            if(amount > 0)
+            {
+                Balance += amount;
+            }
+        }
+
+        //Method to withdraw money
+        public bool Withhdraw(double amount)
+        {
+            if(amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
